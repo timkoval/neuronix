f0f1908168a659c7f91e94d8b89945bed1948cbd
Tim Koval <timkoval00@gmail.com>
2023-10-05 17:54:47 +0600

    disable secrets support

--- flake.nix
+++ flake.nix
@@ -345,10 +345,10 @@
 
     # my private secrets, it's a private repository, you need to replace it with your own.
     # use ssh protocol to authenticate via ssh-agent/ssh-key, and shallow clone to save time
-    mysecrets = {
-      url = "git+ssh://git@github.com/ryan4yin/nix-secrets.git?shallow=1";
-      flake = false;
-    };
+    # mysecrets = {
+    #   url = "git+ssh://git@github.com/ryan4yin/nix-secrets.git?shallow=1";
+    #   flake = false;
+    # };
 
     # my wallpapers
     wallpapers = {
