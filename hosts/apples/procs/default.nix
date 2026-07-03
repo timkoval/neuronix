978898b2776a4fe4ff793354327493ac8b1587cc
timkoval <timkoval00@gmail.com>
2026-07-03 12:13:37 +0400

    chore: set HERMES_HOME on procs mac

--- hosts/apples/procs/default.nix
+++ hosts/apples/procs/default.nix
@@ -1,4 +1,4 @@
-{ hostVars, ... }:
+{hostVars, ...}:
 #############################################################
 #
 #  Procs - MacBook Pro 16 M3 Max 36G.
@@ -8,5 +8,10 @@
   networking.hostName = hostVars.hostname;
   networking.computerName = hostVars.hostname;
   system.defaults.smb.NetBIOSName = hostVars.hostname;
+
+  environment.variables = {
+    HERMES_HOME = "/Users/${hostVars.username}/.hermes";
+  };
+
   system.stateVersion = 5; # Did you read the comment?
 }
